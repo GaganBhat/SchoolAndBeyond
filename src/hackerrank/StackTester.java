36f9fb9cdf14f81eda83b1ea6e6587eb7fe879e1
Gagan Bhat <gaganbhat6@gmail.com>
2020-01-21 08:02:46 -0600

    Create StackTester.java

---
+++ src/hackerrank/StackTester.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class StackTester {
+
+    public static void main(String[] args) {
+        Deque<Integer> stack = new LinkedList<>();
+        stack.addAll(stack);
+    }
+
+}
