11c3f04c315ae50e235cc9ad9940006283da6245
Gagan Bhat <gaganbhat6@gmail.com>
2020-01-31 14:18:43 -0600

    Working black screen screen saver lab

--- src/labs/screensaver/ScreenSaverComponent.java
+++ src/labs/screensaver/ScreenSaverComponent.java
@@ -1,141 +1,164 @@
 package labs.screensaver;
+/**
+ * The Screen Saver Component java class is the JComponent extender
+ * that produces, manages and generates the circles based on construction
+ * parameters and random color and speed shifts.
+ *
+ * @author Gagan Bhat
+ * Collaborators: None
+ * Teacher Name: Mrs. Ishman
+ * Period: 3
+ * Due Date: 1/31/2020
+ */
 
-import java.awt.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Random;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
 import javax.swing.JComponent;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.awt.geom.Ellipse2D;
+
 
 public class ScreenSaverComponent extends JComponent
 {
-	// Starting color for drawing
-	private static final Color STARTING_COLOR = Color.magenta;
-	
-	// Min and max values for translations of x and y
-	private static final int MIN_CHANGE = 5;
-	private static final int MAX_CHANGE = 50;
-	private int maxCircles;
-	private int diameter;
-	private int dx;
-	private int dy;
+    // Starting color for drawing
+    private static final Color STARTING_COLOR = Color.magenta;
 
-	private int prev_x;
-	private int prev_y;
+    // Min and max values for translations of x and y
+    private static final int MIN_CHANGE = 5;
+    private static final int MAX_CHANGE = 50;
 
-	private Color randomColor;
+    private static final double maxAlpha = 1;
+    private static final double minAlpha = 0.5;
 
-	private Queue<Circle> circleQueue;
-	private boolean initialStartup = true;
+    private Color color;
+    private int queueLength;
 
-	public ScreenSaverComponent(int max, int diameter, int chgX, int chgY) 
-	{
-		this.maxCircles = max;
-		this.diameter = diameter;
-		this.dx = chgX;
-		this.dy = chgY;
-		circleQueue = new LinkedList<>();
-		setRandomColor();
-	}
+    private int circleDiameter;
+    private int xDirectionVector;
+    private int yDirectionVector;
 
-	/** Add a new circle to be drawn and then draw all circles.
-	 *  @param g the Graphics object for drawing in this component
-	 */
-	@Override
-	public void paintComponent(Graphics g) 
-	{
-		Graphics2D gr2 = (Graphics2D) g;
-		int maxWidth = getWidth();
-		int maxHeight = getHeight();
-		
-		addCircle();
-		drawAll(gr2);
-	}
-	
-	private void drawAll(Graphics2D gr)
-	{
-		if(circleQueue.size() > maxCircles)
-			circleQueue.poll();
+    private int dx;
+    private int dy;
+    private Queue<Circle> circleQueue;
+    private Point previousPoint;
 
-		for(Circle circle : circleQueue)
-			drawCenteredCircle(gr,circle.getUpperLeft().x, circle.getUpperLeft().y,
-					diameter, circle.getColor());
-	}
+    /**
+     * The Screen saver component constructor initializes all the instant variable fields
+     * @param max maximum number of circles on the screen at once
+     * @param diam diameter of circle
+     * @param chgX initial x translation
+     * @param chgY initial y translation
+     */
+    public ScreenSaverComponent(int max, int diam, int chgX, int chgY)
+    {
+        queueLength = max;
+        circleDiameter = diam;
+        dx = chgX;
+        dy = chgY;
+        xDirectionVector = 1;
+        yDirectionVector = 1;
+        previousPoint = null;
+        circleQueue = new LinkedList<>();
 
-	private void drawCenteredCircle(Graphics2D g, int xTransformed, int yTransformed, int r, Color color) {
+    }
 
-		xTransformed = xTransformed-(r/2);
-		yTransformed = yTransformed-(r/2);
-		prev_x = xTransformed;
-		prev_y = yTransformed;
+    /** Add a new circle to be drawn and then draw all circles.
+     *  @param g the Graphics object for drawing in this component
+     */
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D gr2 = (Graphics2D) g;
 
-		g.setColor(color);
-		g.fillOval(xTransformed,yTransformed,r,r);
-	}
-	
-	private void addCircle() 
-	{
-		checkEdgeCollision();
-		if(initialStartup){
-			initialStartup = false;
-			circleQueue.add(
-					new Circle(
-							new Point(getWidth()/2, getHeight()/2),
-							randomColor
-					)
-			);
-		} else {
-			circleQueue.add(
-					new Circle(
-							new Point(prev_x + dx, prev_y + dy),
-							randomColor
-					)
-			);
-		}
+        super.paintComponent(g);
+        gr2.setColor(Color.BLACK);
+        gr2.fillRect(0, 0, getWidth(), getHeight());
 
-	}
+        addCircle();
+        drawAll(gr2);
+    }
 
-	private void setRandomColor(){
-		Random randomGenerator = new Random();
-		int red = randomGenerator.nextInt(256);
-		int green = randomGenerator.nextInt(256);
-		int blue = randomGenerator.nextInt(256);
-		randomColor = new Color(red,green,blue);
-	}
+    /**
+     * Draws all the shape objects in the circle queue
+     * @param gr graphics 2D object from paint component
+     */
+    private void drawAll(Graphics2D gr)
+    {
 
-	public void setRandomDeltas(){
-		Random randomGenerator = new Random();
-		if(dx > 0)
-			this.dx = -(randomGenerator.nextInt(55) - 5);
-		else
+        for(Circle tempCircle : circleQueue)
+        {
+            Point upperLeftPoint = tempCircle.getUpperLeft();
+            Ellipse2D.Double ellipseCircle = new Ellipse2D.Double(
+                    upperLeftPoint.getX(),
+                    upperLeftPoint.getY(),
+                    circleDiameter,
+                    circleDiameter);
+
+            (gr).setColor(tempCircle.getColor());
+            (gr).fill(ellipseCircle);
+        }
+
+    }
 
 
-		this.dy = (randomGenerator.nextInt(55) - 5);
+    /**
+     * The addCircle method is constantly called by the paint component
+     * run loop
+     */
+    private void addCircle()
+    {
+        int maxWidth = getWidth();
+        int maxHeight = getHeight();
 
-		this.dx *= -1;
-		this.dy *= -1;
+        if (previousPoint == null)
+        {
+            previousPoint = new Point((maxWidth + circleDiameter) / 2,
+                    (maxHeight + circleDiameter) / 2);
+            color = STARTING_COLOR;
+            circleQueue.add(new Circle(previousPoint, color));
+        }
+        else
+        {
 
-	}
+            if (circleQueue.size() >= queueLength)
+            {
+                circleQueue.poll();
+            }
 
-	private void checkEdgeCollision(){
-		boolean isTriggered = false;
+            if ((int) previousPoint.getX() + dx * xDirectionVector > getWidth() ||
+                    (int) previousPoint.getX() + dx * xDirectionVector < 0)
+            {
+                xDirectionVector = -1 * xDirectionVector;
+                dx = (int) (Math.random() * (MAX_CHANGE - MIN_CHANGE + 1)) + MIN_CHANGE;
+                color = new Color(
+                        (float)Math.random(),
+                        (float)Math.random(),
+                        (float)Math.random(),
+                        (float) (Math.random() * (maxAlpha - minAlpha) + minAlpha)
+                );
+            }
 
-		if(prev_x + dx > getWidth() || prev_x + dx < -diameter/2) {
-			isTriggered = true;
-			dx *= -1;
-		}
+            if ((int) previousPoint.getY() + dy * yDirectionVector > getHeight() ||
+                    (int) previousPoint.getY() + dy * yDirectionVector < 0)
+            {
+                yDirectionVector = -1 * yDirectionVector;
+                dy = (int) (Math.random() * (MAX_CHANGE - MIN_CHANGE + 1)) + MIN_CHANGE;
+                color = new Color(
+                        (float) Math.random(),
+                        (float) Math.random(),
+                        (float) Math.random(),
+                        (float) (Math.random() * (maxAlpha - minAlpha) + minAlpha)
+                );
+            }
 
+            previousPoint = new Point((int) previousPoint.getX() + dx * xDirectionVector,
+                    (int) previousPoint.getY() + dy * yDirectionVector);
 
-		if(prev_y + dy > getHeight() || prev_y + dy < -diameter/2) {
-			isTriggered = true;
-			dy *= -1;
-		}
+            circleQueue.add(new Circle(previousPoint, color));
+        }
 
-		System.out.println("Queue Size " + circleQueue.size());
-
-		if(isTriggered) {
-			setRandomColor();
-		}
-
-	}
-
+    }
 }
