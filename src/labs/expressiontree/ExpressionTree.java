9f0f7b565079efa5740882a7fbade84f8890abcc
Gagan Bhat <gaganbhat6@gmail.com>
2020-03-02 11:16:03 -0600

    Added expression tree lab and fixed project directory for git

---
+++ src/labs/expressiontree/ExpressionTree.java
@@ -0,0 +1,36 @@
+package labs.expressiontree;
+
+import java.util.Scanner;
+
+public class ExpressionTree 
+{
+	/** Root of the expression tree */
+	private ExpressionNode root;
+	
+	/** Used to parse a prefix expression */	
+	private Scanner strScan;	
+	
+	public ExpressionTree() 
+	{
+	}
+	
+	public ExpressionTree(String prefix) 
+	{
+	}
+
+	public void setExpression(String prefix) 
+	{
+	}
+
+	public double evaluate() 
+	{
+		return 0.0;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "";
+	}
+
+}
