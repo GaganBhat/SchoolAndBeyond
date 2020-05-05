f54d8e68b46e2a8a41bab22520a0b8d9fbddb948
Gagan Bhat <gaganbhat6@gmail.com>
2020-05-05 12:25:18 -0500

    Added Review class basic functionality

--- src/labs/replit/reviewmanagerproject/Review.java
+++ src/labs/replit/reviewmanagerproject/Review.java
@@ -2,8 +2,57 @@ package labs.replit.reviewmanagerproject;
 
 public class Review {
 
-	public Review(){
+	private String brand;
+	private String productName;
+	private String title;
+	private String content;
+	private int rating;
 
+	public Review(String brand, String productName, String title, String content, int rating){
+		this.brand = brand;
+		this.productName = productName;
+		this.title = title;
+		this.content = content;
+		this.rating = rating;
 	}
 
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
 }
--- src/labs/replit/reviewmanagerproject/ReviewManager.java
+++ src/labs/replit/reviewmanagerproject/ReviewManager.java
@@ -1,12 +1,26 @@
 package labs.replit.reviewmanagerproject;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class ReviewManager {
 
-	public ReviewManager(){
+	HashMap<String, HashSet<Review>> reviewMap;
 
+	public ReviewManager(){
+		reviewMap = new HashMap<>();
+	}
+
+	public ReviewManager(String csvFile){
+		this();
+		loadData(csvFile);
 	}
 
 	public void loadData(String dataPath){
+
+	}
+
+	public void addReview(Review review){
 
 	}
 
