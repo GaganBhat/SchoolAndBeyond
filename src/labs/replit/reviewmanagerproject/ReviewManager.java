7b5a68b81d5ea283f7d47079308bdf73882cdbb5
Gagan Bhat <gaganbhat6@gmail.com>
2020-05-08 00:19:18 -0500

    All 6000 reviews indexed and working

--- src/labs/replit/reviewmanagerproject/ReviewManager.java
+++ src/labs/replit/reviewmanagerproject/ReviewManager.java
@@ -2,10 +2,7 @@ package labs.replit.reviewmanagerproject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class ReviewManager {
 
@@ -31,14 +28,36 @@ public class ReviewManager {
 		assert s != null;
 		s.nextLine();
 
-		String[] reviewData = s.nextLine().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-		System.out.println(Arrays.toString(reviewData));
-
-
+		while (s.hasNextLine()) {
+			String[] reviewData = s.nextLine().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+			if(reviewData.length != 6) {
+				List<String> tempList = new ArrayList<>(List.of(reviewData));
+				tempList.add("5");
+				reviewData = tempList.toArray(reviewData);
+			}
+			addReview(new Review(reviewData));
+		}
 	}
 
 	public void addReview(Review review){
-
+		if(reviewMap.containsKey(review.getBrand()))
+			reviewMap.get(review.getBrand()).add(review);
+		else
+			reviewMap.put(review.getBrand(), new HashSet<>(Collections.singletonList(review)));
 	}
 
+	public String getBrands(){
+		return reviewMap.keySet().toString();
+	}
+
+	public int getNumBrands(){
+		return reviewMap.keySet().size();
+	}
+
+	@Override
+	public String toString() {
+		return "ReviewManager{" +
+				"reviewMap=" + reviewMap +
+				'}';
+	}
 }
