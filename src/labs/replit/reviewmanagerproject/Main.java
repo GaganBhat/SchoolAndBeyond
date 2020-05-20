b5ae16f924bfed14f8b4902b6a8b0205befccf4b
Gagan Bhat <gaganbhat6@gmail.com>
2020-05-19 23:31:52 -0500

    Finalised sent final HCS3 Project

--- src/labs/replit/reviewmanagerproject/Main.java
+++ src/labs/replit/reviewmanagerproject/Main.java
@@ -1,7 +1,23 @@
 package labs.replit.reviewmanagerproject;
+/**
+ * Creates a Review Manager and inputs review data from a real-life dataset.
+ * Tests the various functions of the Review Manager. It uses Java
+ * assertions and does unit tests for the functionality of the map
+ * of sets containing the review objects.
+ *
+ * @author Gagan Bhat
+ * Collaborators: None
+ * Teacher Name: Mrs. Ishman
+ * Period: 3
+ * Due Date: 5/18/2020
+ */
 
 public class Main {
 
+	/**
+	 * Main method that tests review manager with sample data set.
+	 * @param args java runtime args for the program
+	 */
 	public static void main(String[] args) {
 		ReviewManager reviewManager = new ReviewManager("review_data.csv");
 
@@ -11,21 +27,31 @@ public class Main {
 		System.out.println("Total Number of Brands : " + reviewManager.getNumBrands());
 
 
-		System.out.println("Average Toshiba Rating : " + reviewManager.getAverageRating("Toshiba"));
-		System.out.println("Average Non Existent Brand Rating : " + reviewManager.getAverageRating("Hello"));
+		System.out.println("Average Toshiba Rating : "
+				+ reviewManager.getAverageRating("Toshiba"));
+
+		System.out.println("Average Non Existent Brand Rating : "
+				+ reviewManager.getAverageRating("Hello"));
+
 		System.out.println();
 
 		System.out.println("--------------------------------");
-		System.out.println("Number of Reviews BEFORE adding extra review - " + reviewManager.getTotalReviews());
+		System.out.println("Number of Reviews BEFORE adding extra review - "
+				+ reviewManager.getTotalReviews());
 		System.out.println("Adding review...");
-		reviewManager.addReview(new Review("NewBrand", "RandomProduct", "title", "randomContent", "uniqueUser", 5));
-		System.out.println("Number of Reviews AFTER adding extra review - " + reviewManager.getTotalReviews());
+		reviewManager.addReview(new Review("NewBrand",
+				"RandomProduct", "title",
+				"randomContent", "uniqueUser", 5));
+		System.out.println("Number of Reviews AFTER adding extra review - "
+				+ reviewManager.getTotalReviews());
 
 		System.out.println("-----------------------w---------");
-		System.out.println("Number of Reviews BEFORE removing a review - " + reviewManager.getTotalReviews());
+		System.out.println("Number of Reviews BEFORE removing a review - "
+				+ reviewManager.getTotalReviews());
 		System.out.println("Removing review from username NvlSailDriver");
 		reviewManager.removeReview("NvlSailDriver");
-		System.out.println("Number of Reviews AFTER removing a review - " + reviewManager.getTotalReviews());
+		System.out.println("Number of Reviews AFTER removing a review - "
+				+ reviewManager.getTotalReviews());
 		System.out.println("--------------------------------");
 
 		System.out.println();
--- src/labs/replit/reviewmanagerproject/ReviewManager.java
+++ src/labs/replit/reviewmanagerproject/ReviewManager.java
@@ -5,19 +5,48 @@ import java.io.FileNotFoundException;
 import java.util.*;
 import static java.lang.Double.NaN;
 
+/**
+ * The ReviewManager class tracks and manages a list of Review objects through a
+ * map containing sets as values sorted based on brands
+ * that co-relate to reviews on electronic goods from
+ * e-retailers such as amazon and best buy. It contains the ability to sort
+ * based on the average rating, retrieve average rating for each e-retailer,
+ * retrieve average rating for a specified product, retrieve
+ * individual Review objects based on unique identification usernames.
+ *
+ * @author Gagan Bhat
+ * Collaborators: None
+ * Teacher Name: Mrs. Ishman
+ * Period: 3
+ * Due Date: 5/18/2020
+ */
+
 public class ReviewManager {
 
-	HashMap<String, HashSet<Review>> reviewMap;
+	private final HashMap<String, HashSet<Review>> reviewMap;
 
+	/**
+	 * Initializes an empty hashmap for reviewMap.
+	 */
 	public ReviewManager(){
 		reviewMap = new HashMap<>();
 	}
 
+	/**
+	 * A constructor that calls the loading of data on
+	 * construction of the review manager class
+	 * @param csvFile file to load review data from
+	 */
 	public ReviewManager(String csvFile){
 		this();
 		loadData(csvFile);
 	}
 
+	/**
+	 * Loads data into the reviewMap hashmap using helper methods
+	 * through the data from the file
+	 * @param dataPath file path for the file to load data from
+	 */
 	public void loadData(String dataPath){
 		long startTime = System.nanoTime();
 
@@ -48,6 +77,10 @@ public class ReviewManager {
 		System.out.println();
 	}
 
+	/**
+	 * Adds a review to the existing review map.
+	 * @param review review instance to add
+	 */
 	public void addReview(Review review){
 		if(reviewMap.containsKey(review.getBrand()))
 			reviewMap.get(review.getBrand()).add(review);
@@ -55,14 +88,26 @@ public class ReviewManager {
 			reviewMap.put(review.getBrand(), new HashSet<>(Collections.singletonList(review)));
 	}
 
+	/**
+	 * Gets all the unique brands in all reviews combined
+	 * @return list of brands
+	 */
 	public String getBrands(){
 		return reviewMap.keySet().toString();
 	}
 
+	/**
+	 * Number of unique brands in the reviews combined
+	 * @return unique number of brands
+	 */
 	public int getNumBrands(){
 		return reviewMap.keySet().size();
 	}
 
+	/**
+	 * Gets total reviews in the map
+	 * @return total number of reviews
+	 */
 	public int getTotalReviews(){
 		int total = 0;
 		for(String key : reviewMap.keySet())
@@ -71,6 +116,11 @@ public class ReviewManager {
 		return total;
 	}
 
+	/**
+	 * Average rating for a specific brand
+	 * @param brand brand to check average rating for
+	 * @return the average rating across all reviews for a single brand
+	 */
 	public double getAverageRating(String brand){
 		if(!reviewMap.containsKey(brand)) {
 			return NaN;
@@ -82,12 +132,21 @@ public class ReviewManager {
 		return total / reviewMap.get(brand).size();
 	}
 
+	/**
+	 * Removes a review based on the unique username who wrote it
+	 * @param uniqueUsername review to remove based on username
+	 */
 	public void removeReview(String uniqueUsername){
 		for(String key : reviewMap.keySet())
 			reviewMap.get(key).removeIf(review ->
 					review.getUniqueUsername().equals(uniqueUsername));
 	}
 
+	/**
+	 * Gets the review based on unique username
+	 * @param uniqueUsername unique username of review to retrieve
+	 * @return review created by the username
+	 */
 	public Review getReview(String uniqueUsername) {
 		for(String key : reviewMap.keySet())
 			for(Review review : reviewMap.get(key))
@@ -96,6 +155,17 @@ public class ReviewManager {
 		return null;
 	}
 
+	/**
+	 * Deletes all reviews in the review map and empties it.
+	 */
+	public void deleteAllReviews(){
+		reviewMap.clear();
+	}
+
+	/**
+	 * Prints the review map in its entirety.
+	 * @return string with review map info
+	 */
 	@Override
 	public String toString() {
 		return "ReviewManager{" +
